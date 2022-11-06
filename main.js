1e0e0fb813a6c74f974dbcd5fb5bae30fd91f046
skiotto93 <fabio.forenza@hotmail.com>
2022-11-06 16:58:56 +0100

    Boo

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 "use strict";
+
+
+/*----------------
+    FUNCTIONS
+----------------*/
+// Funzione che restituisce un numero intero random 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+/*----------------
+    MAIN
+----------------*/
 const boardContainer = document.querySelector(".board");
 
-
 const start = document.querySelector(".start-button");
+
 start.addEventListener('click', function () {
-    for ( let i = 1; i <= 100; i++ ) {
+    for( let i = 1; i <= 100; i++ ) {
         const boardCell = document.createElement("div");
         boardCell.innerHTML = i;
-        boardCell.classList.add("board-cell-100");
+        boardCell.classList.add("board-cell");
         boardCell.addEventListener('click', function() {
-            boardCell.classList.add("color-add")
-            console.log(this.innerHTML);
-        }       
+            boardCell.classList.add("color-add");
+        }
         );            
         boardContainer.append(boardCell);
     };
+    const listBomb = [];
+    while( listBomb.length < 16 ) {
+        const numberRandom = getRndInteger(1, 100);
+        if( listBomb.includes(numberRandom) === false ) {
+            listBomb.push(numberRandom);       
+        }
+    }
+    const cells = document.querySelectorAll('.board-cell');
+    for( let i = 0; i < cells.length; i++ ) {
+        cell[i].addEventListener('click', function () {
+            console.log(this.innerHTML);
+        })
+    }
+    
 });
-
-//Creo un arrey vuoto ed inserisco 16 numeri casuali(bombe)
-
-const numBombList = [];
-
-for ( let i = 1; i <=16; i++ ) {
-    const numBomb = Math.floor(Math.random() * 100) + 1;
-    numBombList.push = numBomb;
-    console.log(numBomb)
-}
